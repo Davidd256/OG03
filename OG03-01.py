e3c0107992f3f47ea7afdac6e00a287fb0cb3e7a
Davidd256 <davidd256@gmail.com>
2025-01-21 16:11:31 +0300

    Подсчёт площади квадрата в отдельной ветке test.

--- OG03-01.py
+++ OG03-01.py
@@ -3,3 +3,9 @@ def area_circle(r):
     return s
 r = 2
 print(f'Площадь круга радиусом  {r} равна: {area_circle(r)}')
+
+def area_square_area(a):
+    S_s = a ** 2
+    return S_s
+a = 2
+print(f'Площадь квадрата со стороной  {a} равна: {area_square_area(a)}')
